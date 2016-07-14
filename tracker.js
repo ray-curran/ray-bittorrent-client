83f584e22de1f4fa3d765ca3345ba40445f45e4a
ray-curran <rpcurr@gmail.com>
2016-07-14 15:00:56 -0500

    Add announce response function

--- tracker.js
+++ tracker.js
@@ -95,5 +95,24 @@ function buildAnnounceReq(connId, torrent, port=6881) {
 }
 
 function parseAnnounceResp(resp) {
-  // ...
+  function group(iterable, groupSize) {
+    let groups = [];
+    for(let i = 0; i < iterable.length; i += groupSize){
+      groups.push(iterable.slice(i, i + groupSize));
+    }
+    return groups;
+  }
+
+  return {
+    action: resp.readUInt32BE(0),
+    transactionId: resp.readUInt32BE(4),
+    leechers: resp.readUInt32BE(8),
+    seeders: resp.readUInt32BE(12),
+    peers: group(resp.slice(20), 6).map(address => {
+      return {
+        ip: address.slice(0, 4).join('.'),
+        port: address.readUInt16BE(4)
+      }
+    })
+  }
 }
