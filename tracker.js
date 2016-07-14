c420e45189e0ab53b5d0605d17ef36796f359f31
ray-curran <rpcurr@gmail.com>
2016-07-14 14:21:33 -0500

    Add connection response and request functions

--- tracker.js
+++ tracker.js
@@ -33,12 +33,26 @@ function respType(resp) {
   // ...
 }
 
+const crypto = require('crypto');
+
 function buildConnReq() {
-  // ...
+  const buf = Buffer.alloc(16);
+
+  buf.writeUInt32BE(0x417, 0);
+  buf.writeUInt32BE(0x27101980, 4);
+  buf.writeUInt32BE(0, 8);
+
+  crypto.randomBytes(4).copy(buf, 12)
+
+  return buf;
 }
 
 function parseConnResp(resp) {
-  // ...
+  return {
+    action: resp.readUInt32BE(0),
+    transactionId: resp.readUInt32BE(4),
+    connectionId: resp.slice(8)
+  }
 }
 
 function buildAnnounceReq(connId) {
