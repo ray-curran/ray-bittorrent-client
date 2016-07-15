003800f9d82ee139534141292b9687e556532e14
ray-curran <rpcurr@gmail.com>
2016-07-15 15:41:58 -0500

    Add piece functions and clean up downloads

--- src/download.js
+++ src/download.js
@@ -6,21 +6,23 @@ const message = require('./message')
 const tracker = require('./tracker');
 
 module.exports = torrent => {
+  const requested = [];
   tracker.getPeers(torrent, peers => {
-    peers.forEach(peer => download(peer, torrent));
+    peers.forEach(peer => download(peer, torrent, requested));
   });
 };
 
-function download(peer, torrent) {
+function download(peer, torrent, requested) {
   const socket = net.Socket();
   socket.on('error', console.log);
   socket.connect(peer.port, peer.ip, () => {
     socket.write(message.buildHandshake(torrent));
   });
-  onWholeMsg(socket, msg => msgHandler(msg, socket));
+  const queue = [];
+  onWholeMsg(socket, msg => msgHandler(msg, socket, requested, queue));
 }
 
-function msgHandler(msg, socket) {
+function msgHandler(msg, socket, requested) {
   if (isHandshake(msg)) {
     socket.write(message.buildInterested());
   } else {
@@ -28,9 +30,9 @@ function msgHandler(msg, socket) {
 
     if(m.id === 0) chokeHandler();
     if(m.id === 1) unchokeHandler();
-    if(m.id === 4) haveHandler(m.payload);
+    if(m.id === 4) haveHandler(m.payload, socket, requested, queue);
     if(m.id === 5) bitfieldHandler(m.payload);
-    if(m.id === 7) pieceHandler(m.payload);
+    if(m.id === 7) pieceHandler(m.payload, socket, requested, queue);
   }
 }
 
@@ -54,5 +56,24 @@ function onWholeMsg(socket, callback) {
   });
 }
 
+function haveHandler(payload, socket, requested, queue) {
+  const pieceIndex = payload.readUInt32BE(0);
+  queue.push(pieceIndex);
+  if(queue.length === 1) {
+    requestPiece(socket, requested, queue);
+  }
+}
 
+function pieceHandler(payload, socket, requested, queue) {
+  queue.shift();
+  requestPiece(socket, requested, queue);
+}
+
+function requestPiece(socket, requested, queue) {
+  if(requested[queue[0]]) {
+    queue.shift();
+  } else {
+    socket.write(message.buildRequest(pieceIndex));
+  }
+}
 
