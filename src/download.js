bf2ee50b8bed5b34e057950ce623f3f98987fc7b
ray-curran <rpcurr@gmail.com>
2016-07-15 15:23:18 -0500

    Add functionality for message parsing and handling for interested & choke messaging

--- src/download.js
+++ src/download.js
@@ -2,25 +2,57 @@
 
 const net = require('net');
 const Buffer = require('buffer').Buffer;
+const message = require('./message')
 const tracker = require('./tracker');
 
 module.exports = torrent => {
   tracker.getPeers(torrent, peers => {
-    peers.forEach(download);
+    peers.forEach(peer => download(peer, torrent));
   });
 };
 
-function download(peer) {
+function download(peer, torrent) {
   const socket = net.Socket();
   socket.on('error', console.log);
   socket.connect(peer.port, peer.ip, () => {
-
+    socket.write(message.buildHandshake(torrent));
   });
-  socket.on('data', data => {
-
-  });
+  onWholeMsg(socket, msg => msgHandler(msg, socket));
 }
 
+function msgHandler(msg, socket) {
+  if (isHandshake(msg)) {
+    socket.write(message.buildInterested());
+  } else {
+    const m = message.parse(msg);
+
+    if(m.id === 0) chokeHandler();
+    if(m.id === 1) unchokeHandler();
+    if(m.id === 4) haveHandler(m.payload);
+    if(m.id === 5) bitfieldHandler(m.payload);
+    if(m.id === 7) pieceHandler(m.payload);
+  }
+}
+
+function isHandshake(msg) {
+  return msg.length === msg.readUInt8(0) + 49 && msg.toString('utf8', 1) === 'BitTorrent protocol';
+}
+
+function onWholeMsg(socket, callback) {
+  let savedBuf = Buffer.alloc(0);
+  let handshake = true;
+
+  socket.on('data', recvBuf => {
+    const msgLen = () => handshake ? savedBuf.readUInt8(0) + 49 : savedBuf.readInt32BE(0) + 4;
+    savedBuf = Buffer.concat([savedBuf, recvBuf]);
+
+    while (savedBuf.length >= 4 && savedBuf.length >= msgLen()) {
+      callback(savedBuf.slice(0, msgLen()));
+      savedBuf = savedBuf.slice(msgLen());
+      handshake = false
+    }
+  });
+}
 
 
 
